d8213351692edec1b4dc83e6a735ad4f7eb4927c
Alexander Zavazzkii <60303860+enzziwe@users.noreply.github.com>
2021-12-25 00:19:27 +0500

    Add files via upload

---
+++ faq-main/README.md
@@ -0,0 +1,37 @@
+# Горячие клавиши
+#### Выделить всё:
+Ctrl+A
+#### Копировать:
+Ctrl+C
+#### Вставыить:
+Ctrl+V
+#### Сохранить:
+Ctrl+S
+#### Вырезать:
+Ctrl+X 
+#### Отменить:
+Ctrl+Z
+# Работа с git
+#### Скачать проект:
+```bush 
+git clone <URL>
+``` 
+### Учесть  все файлы:
+```bush
+git add .
+```
+#### сохранить  коммит:
+```bush
+git commit -m "..."
+```
+### отправка в облако:
+```bush
+git push
+```
+
+# Ссылки 
+### Git:
+[git-scm.com](https://git-scm.com/)
+### VS code:
+[code.visualstudio.com](https://code.visualstudio.com/)
+
---
+++ faq-main/test/1122_1.js
@@ -0,0 +1,36 @@
+function clickCube(){
+    var white=getRandomInt(225);
+    var purple=getRandomInt(225);
+    var grey=getRandomInt(225);
+    console.log(white, purple, grey);
+    document.getElementById("cube").style.backgroundColor="rgb("+ white +","+ purple +","+ grey + ")"
+
+    var stringBoxShadow =  "0 0 5px rgba("+ white +","+ purple +","+ grey +", 1)," +
+    "0 0 25px rgba("+ white +","+ purple +","+ grey +", 1), " +
+    "0 0 50px rgba("+ white +","+ purple +","+ grey +", 1), " +
+    "0 0 100px rgba("+ white +","+ purple +","+ grey +", .75), " +
+    "0 0 200px rgba("+ white +","+ purple +","+ grey +", .5), " +
+    "0 0 300px rgba("+ white +","+ purple +","+ grey +", .25) "
+
+    var boo=getRandomInt(200);
+    var boob=getRandomInt(200);
+
+    var storonaOne= boo + "px" ;
+    var storonaTwo= boob + "px" ;
+
+    var goop=getRandomInt(225);
+
+    var bloop=getRandomInt(50);
+
+    
+var rad= bloop + "px";
+    document.getElementById("cube").style.border=" 5px solid rgb("+ goop +","+ purple +","+ grey + ")"
+    document.getElementById("cube").style.height=storonaOne;
+    document.getElementById("cube").style.height=storonaOne;
+    document.getElementById("cube").style.width=storonaTwo;
+    document.getElementById("cube").style.borderRadius=rad;
+}
+function getRandomInt(max){
+    return Math.floor(Math.random()*max);
+}
+
